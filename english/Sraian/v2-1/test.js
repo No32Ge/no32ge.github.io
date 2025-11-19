3aa5677655ce995d67d916434b88a901704c07e8
No32Ge <127807254+No32Ge@users.noreply.github.com>
2025-11-20 02:06:07 +0800

    测试子域名绑定

--- english/Sraian/v2-1/test.js
+++
@@ -1,8 +0,0 @@
-import {getLibraryMeta} from "./res/loadRes/index.js"
-
-
-
-export default  async function getLibraryMetaTest(){
-    console.log("得到最终数据:", await getLibraryMeta("http://127.0.0.1:8000/ge32/labs/GeSeed/library/index/libraryIndex.json","wonder"))
-};
-
